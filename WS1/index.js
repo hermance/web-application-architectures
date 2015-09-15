0644c2a8e72a5adbc1e2f3c5cac300f63e1ebc7e
hermance <hermancedornier@gmail.com>
2015-09-15 15:34:32 +0200

    exo 4
    exo 4 fini

--- WS1/index.js
+++ WS1/index.js
@@ -207,6 +207,59 @@ for(var i=0; i<data2.rentals.length;i++)
 
 
 
+document.write('<br/><br/>EXERCICE 4 <br/');
+for(var i=0; i<data2.rentals.length;i++)
+{
+	for(var j=0; j<data2.cars.length; j++){
+		if(data2.rentals[i].carId == data2.cars[j].id)
+		{
+			var returnDate = new Date(data2.rentals[i].returnDate);
+			var pickupDate = new Date(data2.rentals[i].pickupDate);
+			var temps = ((returnDate - pickupDate)/86400000)+1;
+			//alert(temps);
+			if(temps >= 1 && temps<4){
+				var prix = ((data2.cars[j].pricePerDay )  * temps)
+				+ (data2.cars[j].pricePerKm * data2.rentals[i].distance);
+				prix = prix - 0.1*prix;
+				var insurance= prix /2;
+				var roadsideAssistance = 1*temps;
+				var drivy = prix - insurance - roadsideAssistance;
+				var charge = (temps *4);
+				var drivy2 = prix - insurance - roadsideAssistance - charge;
+				}
+				else if (temps >= 4 && temps<10){
+				var prix = ((data2.cars[j].pricePerDay )  * temps)
+				+ (data2.cars[j].pricePerKm * data2.rentals[i].distance);
+				prix = prix - 0.3*prix;
+				var insurance= prix /2;
+				var roadsideAssistance = 1*temps;
+				var drivy = prix - insurance - roadsideAssistance;
+				var charge = (temps *4);
+				var drivy2 = prix - insurance - roadsideAssistance - charge;
+				}
+				else if (temps >= 10){
+				var prix = ((data2.cars[j].pricePerDay )  * temps)
+				+ (data2.cars[j].pricePerKm * data2.rentals[i].distance);
+				prix = prix - 0.5*prix;
+				var insurance= prix /2;
+				var roadsideAssistance = 1*temps;
+				var drivy = prix - insurance - roadsideAssistance;
+				var charge = (temps *4);
+				var drivy2 = prix - insurance - roadsideAssistance - charge;
+				}
+				document.write('<br/> .........................');
+			document.write('</br>Le prix avec réduction est de : '  + prix+'€ <br/>');
+			document.write('</br>Le prix de l\'assurance est de : '  + insurance+'€ <br/>');
+			document.write('</br>Le prix de l\'assistance est de : '  + roadsideAssistance+'€ <br/>');
+			document.write('</br>Le prix du drivy est de : '  + drivy+'€ <br/>');
+			document.write('</br>Le prix des charges est de : '  + charge+'€ (pour réduction en cas d\'accident) <br/>');
+			document.write('</br>Le prix du drivy avec l\'assurance est de : '  + drivy2+'€ <br/>');
+			//alert(prix);
+			//return prix; 
+			//on a relié voiture et proprio
+		}
+	}
+}
 
 
 
